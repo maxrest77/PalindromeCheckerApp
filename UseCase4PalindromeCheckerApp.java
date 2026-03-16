9fea927f22d2d92ee25cfc8a06742fbb1d8b5d41
Karthikeyan S <karthik71011@gmail.com>
2026-03-16 09:11:46 +0530

    Implement palindrome checker application

---
+++ UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
+
+        // Convert string to character array
+        char[] characters = input.toCharArray();
+
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
+
+        // Two-pointer comparison
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
+    }
+}
